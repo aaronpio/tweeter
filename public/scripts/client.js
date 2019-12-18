ef8e8cacbdf953ff161d1ce714ab055dcb2bdb2c
Aaron <aaronpiolang@gmail.com>
2019-12-18 22:49:55 +0000

    used ajax requests in tweeter app

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,13 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  }
-];
+$(document).ready(() => {
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-const createTweetElement = tweetObj => {
-  const htmlInjection = `
+  //----------------------------------------------
+  const createTweetElement = tweetObj => {
+    const htmlInjection = `
   <article>
      <header>
        <div class="profName">
@@ -36,26 +16,111 @@ const createTweetElement = tweetObj => {
        </div>
        <div class="handle">${tweetObj.user.handle}</div>
      </header>
-     <main>${tweetObj.content.text}</main>
+     <main>${escape(tweetObj.content.text)}</main>
      <footer>
        ${tweetObj.created_at}
      </footer>
     </article>
    `;
 
-  return $(htmlInjection)[0];
-  //return jQuery.parseHTML(htmlInjection)[1];
-};
+    return $(htmlInjection)[0];
+  };
 
-$(document).ready(() => {
+  //-----------------------------------------------------
+
+  const renderTweet = tweet => {
+    let tweetElement = createTweetElement(tweet);
+    $(".tweetList").prepend(tweetElement);
+  };
+  //-----------------------------------------------------
+
   const renderTweets = tweets => {
     for (const tweet of tweets) {
       let tweetElement = createTweetElement(tweet);
-      $("tweetList").append("heyoyoyoyoyoyoyoyoyoy");
-      $(".tweetList").append(tweetElement);
-      console.log(tweetElement);
+      $(".tweetList").prepend(tweetElement);
     }
   };
 
-  renderTweets(data);
+  //-----------------------------------------------------
+  //Function can take in GET or POST requests and handle them with AJAX
+  const requestTweet = (method, url, data) => {
+    $.ajax({
+      method,
+      url,
+      data
+    })
+      .done(function(result) {
+        if (result) {
+          //does nothing for POSTS, rendersTweets for GET
+          renderTweet(result.slice(-1)[0]);
+        }
+      })
+      .fail(function(error) {
+        console.log("Error: " + error);
+      })
+      .always(function() {
+        console.log("request completed");
+      });
+  };
+
+  //-----------------------------------------------------
+  //Function can take in GET or POST requests and handle them with AJAX
+  const requestTweets = (method, url, data) => {
+    $.ajax({
+      method,
+      url,
+      data
+    })
+      .done(function(result) {
+        if (result) {
+          //does nothing for POSTS, rendersTweets for GET
+          renderTweets(result);
+        } else {
+          requestTweet("GET", "/tweets");
+        }
+      })
+      .fail(function(error) {
+        console.log("Error: " + error);
+      })
+      .always(function() {
+        console.log("request completed");
+      });
+  };
+
+  requestTweets("GET", "/tweets");
+
+  //-----------------------------------------------------
+
+  $("#tweetForm").on("submit", function(event) {
+    event.preventDefault();
+    const charCountValue =
+      $(this)
+        .val("text")
+        .serialize().length - 5;
+
+    if (charCountValue > 140) {
+      alert("Your tweet is too long");
+    } else if (charCountValue === 0) {
+      alert("You haven't entered a tweet");
+    } else {
+      const input = $(this)
+        .val("text")
+        .serialize();
+
+      requestTweets("POST", "/tweets", input);
+
+      //clear the input field
+      $($(this).children()[0]).val("");
+    }
+  });
+
+  //-----------------------------------------------------
+
+  // $(".scrollBtn").on("click", () => {
+  //   $("html, body").animate({
+  //     scrollTop: parseInt($(".new-tweet").offset().top)
+  //   });
+  // });
+
+  //need to work with this^^^^^^^^^^^^^^^^^^^^
 });
